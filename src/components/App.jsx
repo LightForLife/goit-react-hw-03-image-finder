39fdaffa49362526b85c006686270a4bc01156be
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2022-10-05 20:08:35 +0300

    add axios

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,43 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import axios from 'axios';
+import { Component } from 'react';
+
+const params = {
+  key: '29563076-116975c46708de5d99dfe50c3',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  per_page: 12,
 };
+
+export class App extends Component {
+  state = {
+    searchQuery: '',
+    currentPage: 1,
+  };
+
+  async componentDidMount() {
+    try {
+      const url = `https://pixabay.com/api/?q=${this.state.searchQuery}&page=${this.state.searchQuery}`;
+
+      const responce = await axios.get(url, { params });
+      console.log(responce.data.hits);
+    } catch (error) {}
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        React homework template
+      </div>
+    );
+  }
+}
